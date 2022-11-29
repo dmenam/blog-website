d0ceb32c08be1a0877d7c20430476bc644a7353b
David Mena <dmenam@ho1a.com>
2022-11-29 16:49:54 -0600

    Integration with MongoDB - Save blogs in database - Footer copyright

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash/string');
+const mongoose = require("mongoose");
+const { find } = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +18,24 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-//Global Variables
-let posts = [];
+//Datase
+connectDatabase().catch(err => console.log("Connection to database failed:" + err));
 
-app.get("/", (req,res) => {
+//Schemas
+const postSchema = new mongoose.Schema({
+  title: String,
+  content: String
+});
+
+//Models
+const Post = mongoose.model("Post", postSchema);
+
+
+app.get("/",async (req,res) => {
+
+  //console.log(await getPosts());
+  const posts = await getPosts();
+
   res.render("home",
   {
     homeParagraph: homeStartingContent,
@@ -40,32 +56,68 @@ app.get("/compose", (req,res) => {
 });
 
 app.post("/compose", (req,res) => {
-  const blog = {
+  
+  const post = new Post({
     title: req.body.title,
     content: req.body.text
-  }
-  posts.push(blog);
+  });
+
+  post.save();
+
   res.redirect("/");
 });
 
-app.get("/posts/:post", (req,res) =>{
-  const urlParam = _.lowerCase(req.params.post);
+app.get("/posts/:id",async (req,res) =>{
+  
+  const postId = req.params.id;
+  
+  const post = await getPostById(postId);
 
-  posts.forEach((post) => {
-    const postTitle = _.lowerCase(post.title);
-
-    if (postTitle === urlParam) {
-      res.render("post",
-      {
-        post: post,
-      });
-    } else {
-      console.log("Not Found!");
-    }
+  //Render the post
+  res.render("post",
+  {
+    post: post,
   });
-
 });
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
+
+async function connectDatabase() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/blog');
+  console.log("Connection to database established");
+}
+
+async function getPosts(){
+  const posts = await Post.find({});
+  //console.log(posts);
+  return posts;
+}
+
+async function getPostById(id){
+
+  if(mongoose.Types.ObjectId.isValid(id)){
+    post = await Post.findById(id).exec();
+    return post;
+  }
+  else{
+    //Render not found page
+    console.log("Not found");
+    return {title:"Not Found", content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Commodo nulla facilisi nullam vehicula ipsum. Auctor neque vitae tempus quam pellentesque nec nam. Imperdiet massa tincidunt nunc pulvinar sapien et. Congue eu consequat ac felis donec et. Sit amet aliquam id diam maecenas ultricies. Diam phasellus vestibulum lorem sed risus. Etiam dignissim diam quis enim lobortis scelerisque fermentum dui. Vulputate dignissim suspendisse in est ante in. Eu mi bibendum neque egestas."};
+  }
+}
+
+async function getPostByName(postName){
+
+  post = await Post.findOne({title: postName}).exec();
+  
+  if(post){
+    return post;
+  }
+  else{
+    //Render not found page
+    console.log("Not found");
+    return {title:"Not Found", content: "This blog does not exist or was removed"};
+  }
+}
